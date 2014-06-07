4dc49f64cf14a27c0391e1808d1a517a36feae6d
craig3050 <craig3050@gmail.com>
2014-06-07 21:15:59 +0100

    Run action from address bar
    Uses action such as http://localhost/24 to count up to 24.

---
+++ Test1.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/<number>")
+def hello(number):
+
+    templateData = {
+      'title' : "All the numbers up to...",
+      'numbers' : give_number(number)
+    }
+
+    return render_template('Test1.html', **templateData)
+
+def give_number(number):
+    start = 1
+    numbers = []
+    while start != int(number):
+        numbers.append(start)
+        start += 1
+    print numbers
+    final = "= "
+    for item in numbers:
+        final = final + str(item) + ", "
+        print final
+    return final
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=80, debug=True)
